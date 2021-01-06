41770df4dfe982e6b88d83e67ea72751dd39e3e9
Patricija Brečko <patricija.brecko@gmail.com>
2021-01-06 12:48:00 +0100

    Domov placeholder

--- src/views/Domov.vue
+++ src/views/Domov.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <!--<img alt="Vue logo" src="../assets/logo.png">-->
-    <HelloWorld msg="Hello"/>
-  </div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1 class="pt-4 text-center">Dan odprtega znanja</h1>
+        <h3 class="pb-4 text-center">13.2.2021, 10.00-18.00</h3>
+      </b-col>
+    </b-row>
+    <b-row class="justify-content-center">
+      <b-col cols="8">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+          architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+          sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+          voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+          amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+          labore et dolore magnam aliquam quaerat voluptatem.
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-  name: 'Domov',
-  components: {
-    HelloWorld,
-  },
-};
 </script>
+
+<style scoped lang="scss">
+
+p {
+  font-size: 1.2rem;
+}
+
+</style>
